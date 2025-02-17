0fe41cba42c9a2c10233c8d611755ee9f660859a
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-02-17 18:05:18 +0300

    Initial Commit

---
+++ Tispy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tispy
+//
+//  Created by Muhammed Mustafa Geldi on 17.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TispyTests/TispyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TispyTests.swift
+//  TispyTests
+//
+//  Created by Muhammed Mustafa Geldi on 17.02.2025.
+//
+
+import Testing
+@testable import Tispy
+
+struct TispyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
